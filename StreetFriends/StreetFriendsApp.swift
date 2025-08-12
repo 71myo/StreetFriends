0739c0bfde178885bd85b1a6b1d9992bcc2449aa
Hyojeong <abadeer@naver.com>
2025-08-13 00:14:29 +0900

    Initial Commit

---
+++ StreetFriends/StreetFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetFriendsApp.swift
+//  StreetFriends
+//
+//  Created by Hyojeong on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StreetFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
