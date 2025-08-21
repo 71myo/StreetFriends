648566156c279982cb8716af676a06cac485b5f6
Hyojeong <abadeer@naver.com>
2025-08-21 22:16:57 +0900

    ✨ feat: SwiftData 모델 구조 설정

--- StreetFriends/Models/Cat.swift
+++ StreetFriends/Models/Cat.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Cat {
+    @Attribute(.unique) var id: UUID  = UUID()
+    
+    var name: String
+    var creationDate: Date
+    
+    @Relationship(deleteRule: .cascade, inverse: \Encounter.cat)
+    var encounters: [Encounter] = []
+    
+    init(name: String) {
+        self.name = name
+        self.creationDate = Date()
+    }
+}
--- StreetFriends/Models/Encounter.swift
+++ StreetFriends/Models/Encounter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Encounter: Identifiable {
+    @Attribute(.unique) var id: UUID = UUID()
+    
+    var date: Date
+    var note: String // 만남 일지
+    
+    @Attribute(.externalStorage)
+    var photo: Data?
+    
+    var cat: Cat?
+    
+    init(date: Date, note: String, photo: Data?) {
+        self.date = date
+        self.note = note
+        self.photo = photo
+    }
+}
