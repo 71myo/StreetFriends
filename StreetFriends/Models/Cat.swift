bcc9379fb2f3562b02e779a53206fb949e1ac158
Hyojeong <abadeer@naver.com>
2025-08-21 23:44:34 +0900

    ♻️ refactor: Cat 모델에 대표 사진 관계 설정

--- StreetFriends/Models/Cat.swift
+++ StreetFriends/Models/Cat.swift
@@ -13,10 +13,16 @@ final class Cat {
     @Attribute(.unique) var id: UUID  = UUID()
     
     var name: String
-    var creationDate: Date
+    var creationDate: Date // 정렬용
+    
+    var representativeEncounter: Encounter? // 대표 Encounter(대표 사진 선택용)
     
     @Relationship(deleteRule: .cascade, inverse: \Encounter.cat)
     var encounters: [Encounter] = []
+    
+    var representativePhoto: Data? {
+        representativeEncounter?.photo
+    }
     
     init(name: String) {
         self.name = name
