bb15a1f995cd0e8e68c7c1fb9bccfbaf082a4dd3
Hyojeong <abadeer@naver.com>
2025-08-28 21:34:25 +0900

    ✨ feat: 데이터 없는 화면 UI 구현

---
+++ StreetFriends/Views/Screens/EmptyCatView.swift
@@ -0,0 +1,63 @@
+//
+//  EmptyCatView.swift
+//  StreetFriends
+//
+//  Created by Hyojeong on 8/28/25.
+//
+
+import SwiftUI
+
+struct EmptyCatView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            NavigationBar(
+                title: "친구들",
+                leading: {},
+                trailing: {
+                    HStack(spacing: 12) {
+                        Button { } label: {Image(.search).foregroundStyle(.netural80)}
+                        Button { } label: {Image(.addCat).foregroundStyle(.netural80)}
+                    }
+                }
+            )
+            
+            VStack {
+                HStack(alignment: .bottom) {
+                    Text("또는 버튼 누르기")
+                        .font(.pretendard(.medium, size: 18))
+                        .foregroundStyle(.netural40)
+                        .offset(y: 8)
+                    
+                    Image(.arrowToPlus)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.top, 11)
+                
+                Spacer()
+                
+                Image(.mysteryCat)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 142, height: 142)
+                
+                Text("화면을 왼쪽으로 당겨\n친구를 추가해 보세요.")
+                    .font(.pretendard(.medium, size: 18))
+                    .foregroundStyle(.netural40)
+                    .padding(.top, 26)
+                
+                Spacer()
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+        } //: VSTACK
+        .background(
+            Image(.homeBackground)
+                .resizable()
+                .scaledToFill()
+        )
+    }
+}
+
+#Preview {
+    EmptyCatView()
+}
