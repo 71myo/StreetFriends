3764f3f175a7b6923f9b1dc3de538168b13fcd6b
Hyojeong <abadeer@naver.com>
2025-08-29 16:57:36 +0900

    🎨 style: LaunchScreen 배경 에셋 병합 및 좌표 조정

---
+++ StreetFriends/Views/Screens/AddCatChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCatChoiceView.swift
+//  StreetFriends
+//
+//  Created by Hyojeong on 8/29/25.
+//
+
+import SwiftUI
+
+struct AddCatChoiceView: View {
+    var body: some View {
+
+    }
+}
+
+#Preview {
+    AddCatChoiceView()
+}
--- StreetFriends/Views/Screens/LaunchView.swift
+++ StreetFriends/Views/Screens/LaunchView.swift
@@ -13,61 +13,53 @@ struct LaunchView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                // 배경 이미지
                 Image(.imgBackground)
                     .resizable()
                     .scaledToFill()
                     .ignoresSafeArea()
                 
-                Image(.imgMountain)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: geometry.size.width * 1.3)
-                    .shadow(color: .black.opacity(0.10), radius: 2, x: 0, y: -2)
-                    .offset(x: geometry.size.width * -0.2, y: geometry.size.height * 0.18)
-                
                 LaunchAssetView(geometry: geometry, imageName: "effectExclamation",
-                                widthMultiplier: 0.045, xOffsetMultiplier: -0.44, yOffsetMultiplier: -0.72,
+                                widthMultiplier: 0.045, xOffsetMultiplier: -0.3, yOffsetMultiplier: -0.55,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
 
                 LaunchAssetView(geometry: geometry, imageName: "imgCat1",
-                                widthMultiplier: 0.65, xOffsetMultiplier: -0.48, yOffsetMultiplier: -0.59,
+                                widthMultiplier: 0.65, xOffsetMultiplier: -0.35, yOffsetMultiplier: -0.42,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
 
                 LaunchAssetView(geometry: geometry, imageName: "effectBrackets",
-                                widthMultiplier: 0.45, xOffsetMultiplier: 0.1, yOffsetMultiplier: -0.52,
+                                widthMultiplier: 0.45, xOffsetMultiplier: 0.27, yOffsetMultiplier: -0.33,
                                 rotationDegrees: -3, isAnimating: $isAnimating)
 
                 LaunchAssetView(geometry: geometry, imageName: "imgCat2",
-                                widthMultiplier: 0.65, xOffsetMultiplier: 0.19, yOffsetMultiplier: -0.58,
+                                widthMultiplier: 0.65, xOffsetMultiplier: 0.35, yOffsetMultiplier: -0.4,
                                 rotationDegrees: -3, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "imgStreetText",
-                                widthMultiplier: 0.65, xOffsetMultiplier: -0.25, yOffsetMultiplier: -0.35,
+                                widthMultiplier: 0.65, xOffsetMultiplier: -0.1, yOffsetMultiplier: -0.15,
                                 rotationDegrees: 1, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "imgFriendsText",
-                                widthMultiplier: 0.6, xOffsetMultiplier: 0, yOffsetMultiplier: -0.25,
+                                widthMultiplier: 0.6, xOffsetMultiplier: 0.14, yOffsetMultiplier: -0.05,
                                 rotationDegrees: 1, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "imgCat3",
-                                widthMultiplier: 0.5, xOffsetMultiplier: -0.4, yOffsetMultiplier: -0.21,
+                                widthMultiplier: 0.48, xOffsetMultiplier: -0.25, yOffsetMultiplier: -0.01,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "effectZz",
-                                widthMultiplier: 0.07, xOffsetMultiplier: -0.55, yOffsetMultiplier: -0.15,
+                                widthMultiplier: 0.07, xOffsetMultiplier: -0.4, yOffsetMultiplier: 0.06,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "imgCat4",
-                                widthMultiplier: 0.6, xOffsetMultiplier: -0.48, yOffsetMultiplier: 0.1,
+                                widthMultiplier: 0.6, xOffsetMultiplier: -0.33, yOffsetMultiplier: 0.27,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "effectWave",
-                                widthMultiplier: 0.1, xOffsetMultiplier: -0.18, yOffsetMultiplier: 0.15,
+                                widthMultiplier: 0.1, xOffsetMultiplier: -0.03, yOffsetMultiplier: 0.33,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
                 
                 LaunchAssetView(geometry: geometry, imageName: "imgCat5",
-                                widthMultiplier: 0.8, xOffsetMultiplier: 0.12, yOffsetMultiplier: 0.19,
+                                widthMultiplier: 0.77, xOffsetMultiplier: 0.27, yOffsetMultiplier: 0.37,
                                 rotationDegrees: 3, isAnimating: $isAnimating)
             } //: ZSTACK
             .onAppear {
