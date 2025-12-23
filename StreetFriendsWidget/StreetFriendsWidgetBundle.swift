af7676667bd57fefa5cf7d70f14bdb724662a535
Hyojeong <abadeer@naver.com>
2025-12-23 15:35:25 +0900

    ✨ feat: 위젯 추가

---
+++ StreetFriendsWidget/StreetFriendsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StreetFriendsWidgetBundle.swift
+//  StreetFriendsWidget
+//
+//  Created by Hyojeong on 12/23/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StreetFriendsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StreetFriendsWidget()
+    }
+}
